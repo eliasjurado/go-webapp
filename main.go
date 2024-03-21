265f5565d11ce0be776661342483f08ecd6b2fd8
Elias Jurado <ridimdev@gmail.com>
2024-03-21 17:36:23 -0500

    pagina disponible en http://localhost:8080/view/TestPage

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	"gowiki/page"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	p1, _ := page.NewPage("TestPage", "Esta es una página de muestra.")
+	p1.Save()
 
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "¡Hola! Me encantan los %s", r.URL)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := page.LoadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
 }
