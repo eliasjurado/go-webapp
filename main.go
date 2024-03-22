57fa3a940ad19de77f11595c6ca842f51cf3e8fb
Elias Jurado <ridimdev@gmail.com>
2024-03-22 11:27:51 -0500

    visualiza y edita textos

--- main.go
+++ main.go
@@ -1,19 +1,79 @@
 package main
 
 import (
-	"fmt"
-	"gowiki/page"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/view/", ViewHandler)
+	http.HandleFunc("/edit/", EditHandler)
+	http.HandleFunc("/save/", SaveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func NewPage(
+	title string,
+	body string,
+) (obj *Page, err error) {
+	obj = &Page{
+		Title: title,
+		Body:  []byte(body),
+	}
+	return
+}
+
+func (p *Page) Save() error {
+	filename := "pages/" + p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func LoadPage(title string) (*Page, error) {
+	filename := "pages/" + title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := page.LoadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
+	p, err := LoadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+	}
+	tp := "view"
+	RenderView(tp, w, p)
+}
+
+func EditHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	tp := "edit"
+	RenderView(tp, w, p)
+}
+
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.Save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func RenderView(tmplate string, w http.ResponseWriter, p *Page) {
+	tp := "templates/" + tmplate + ".html"
+	t, _ := template.ParseFiles(tp)
+	t.Execute(w, p)
 }
--- page/page.go
+++ page/page.go
@@ -22,12 +22,3 @@ func (p *Page) Save() error {
 	filename := "pages/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
-
-func LoadPage(title string) (*Page, error) {
-	filename := "pages/" + title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
